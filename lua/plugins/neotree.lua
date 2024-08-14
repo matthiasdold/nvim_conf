63ee54f23c31ae1f56d3243b1cd45cb6ba65bf1a
matthiasdold <matthias.dold@gmx.net>
2024-08-14 14:59:55 +0200

    added neotree cfg

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,27 @@
+-- Adding follow for better inspection of python modular structure
+
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      require("neo-tree").setup({
+        default_component_configs = {
+          filesystem = {
+            follow_current_file = {
+              enabled = true, -- This will find and focus the file in the active buffer every time
+              --               -- the current file is changed while the tree is open.
+              leave_dirs_open = false, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+            },
+          },
+        },
+      })
+    end,
+  },
+}
