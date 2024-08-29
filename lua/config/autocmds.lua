2f19efd423a06e72e0ac5b8d327a9ea8d44552bd
matthiasdold <matthias.dold@gmx.net>
2024-08-29 10:57:55 +0200

    Pre nvim10 adjustments

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -36,5 +36,19 @@ vim.api.nvim_create_autocmd("FileType", {
     vim.api.nvim_command("set wrap")
   end,
 })
+
+-- Autoformat on save
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("lsp", { clear = true }),
+  callback = function(args)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = args.buf,
+      callback = function()
+        vim.lsp.buf.format {async = false, id = args.data.client_id }
+      end,
+    })
+  end
+})
+
 --
 -- vim.cmd([[autocmd BufWritePre <t buffer> lua vim.lsp.buf.format()]])
