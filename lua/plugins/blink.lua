433b2299b16e71c2e447a88dc6a254d57946b974
matthiasdold <matthias.dold@gmx.net>
2025-03-01 14:02:58 +0100

    deactivating copilot

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -65,7 +65,7 @@ return {
       sources = {
         -- adding any nvim-cmp sources here will enable them
         -- with blink.compat
-        compat = {},
+
         default = { "lsp", "path", "snippets", "buffer" },
         -- cmdline = {},
       },
